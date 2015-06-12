5d345ab883593a9ea79d4d0eb7fa9dc3c105aba9
Jonathan Laster <jonathan@laster.co>
2015-06-11 21:40:25 -0400

    Retired some functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+
+function getCurrentYear() {
+	var currentYear = (new Date).getFullYear();
+
+	return currentYear;
+}
+
+/*
 function getGreeting() {
 	var greetings = ['Hola', 'Hello', 'Aloha', 'Shalom', 'Namastē', 
 					'Kon\'nichiwa', 'Olá', 'Nǐ hǎo', 'Ahoj', 'Bonjour'];
@@ -6,12 +14,6 @@ function getGreeting() {
 	return greeting;
 }
 
-function getCurrentYear() {
-	var currentYear = (new Date).getFullYear();
-
-	return currentYear;
-} // :)
-
 function getStringColor() {
 	var colorArray = ['#26cad9', '#39c6b8', '#d0a52f', '#e0801f', '#080033', '#000000',
 	 '#307fcf', '#42bdb5', '#798086', '#0f1e24', '#ccc0ad', '#868379', '#381a1a'];
@@ -19,9 +21,10 @@ function getStringColor() {
 
 	return color;
 }
+*/
 
 $(document).ready(function() {
-	$('.string').css('background', getStringColor());
-	$('#greeting').text(getGreeting());
+	//$('.string').css('background', getStringColor());
+	//$('#greeting').text(getGreeting());
 	$('#footerDate').text(getCurrentYear());
 });
